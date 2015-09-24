c20c05ff556b543a886a2572adaefec41d60edf3
Brophy613 <efbrophy@quinnipiac.edu>
2015-09-24 18:25:38 +0000

    Add homepage

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = "home.html"
